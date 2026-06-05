49ba66acbcf73f3897c035fe8de6841dbc85067a
swapnil jain <46972452+swapniljain782@users.noreply.github.com>
2026-06-05 19:30:34 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
-def call(string ProjectName, String ImageTag, string DockerHubUser){
+def call(String ProjectName, String ImageTag, String DockerHubUser){
   sh "docker build -t ${DockerHubUser}/${ProjectName}:${ImageTag} ."
 }
